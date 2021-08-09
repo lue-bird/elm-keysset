97c880789f176e6e8cb05223d3a0dd0d5c3cfa73
passi <pascalfeyer@gmail.com>
2021-08-09 15:56:25 +0200

    - imoprt everything exceptions

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -30,7 +30,7 @@ config =
     , NoSinglePatternCase.rule
     , NoLeftPizza.rule NoLeftPizza.Any
     , NoExposingEverything.rule
-    , NoImportingEverything.rule [ "Nats", "TypeNats" ]
+    , NoImportingEverything.rule []
     , NoMissingTypeAnnotation.rule
     , NoForbiddenWords.rule [ "TODO", "todo" ]
     , NoBooleanCase.rule
